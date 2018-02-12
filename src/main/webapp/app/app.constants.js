854b486060aafa92276e65e81bff362c3cc09cb7
Robert Patch <robert.gpatch@gmail.com>
2018-02-12 01:17:20 -0800

    first commit, everythign works but no new front page yet.

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('patchsheetmetalApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1500348102809)
+        .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
 ;
 })();
